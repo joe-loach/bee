e7aafb935a9579d2ca3fe8b04ca6e54367188d83
Joe <joe.gloach@gmail.com>
2025-03-26 18:26:23 +0000

    Stop safari from zooming on text input

--- src/markup/mod.rs
+++ src/markup/mod.rs
@@ -26,7 +26,7 @@ pub fn head() -> Markup {
         (DOCTYPE)
         head {
             meta charset="utf-8";
-            meta name="viewport" content="width=device-width, initial-scale=1.0";
+            meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1";
 
             title { "Bee Network Tracker" }
 
